fe54ec4ee064e7aff61722317e8be8f7980b23b9
Tainan Bogo <tainanbogo@gmail.com>
2023-03-02 15:31:58 -0300

    pundit booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :flats
+  resources :flats do
+    resources :bookings, only: %i[new create]
+  end
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
