4bf1bda7bb6d37ea463cebec794e09bb1b8b16e8
annedeegan <anne.deegan@outlook.com>
2023-03-02 12:28:19 -0500

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[ show edit ]
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+  end
+
+  private
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+  def booking_params
+    params.require(:booking).permit(:total_price, :start_date, :end_date)
+  end
+
 end
