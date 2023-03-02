48f610e2c68743f8aae37cdd53de5466ffec3e74
Tainan Bogo <tainanbogo@gmail.com>
2023-03-02 17:28:58 -0300

    booking view 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   root to: "pages#home"
+  get "home", to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
